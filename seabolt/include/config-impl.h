01380dcf71613cb2b03b81affd065787f07a3fc7
Ali Ince <ali.ince@codart.co.uk>
2018-01-15 11:23:27 +0000

    Remove redundant conditional defines

--- seabolt/include/config-impl.h
+++ seabolt/include/config-impl.h
@@ -24,12 +24,4 @@
 
 #endif // USE_OPENSSL
 
-#ifndef INET_ADDRSTRLEN
-#define INET_ADDRSTRLEN 16
-#endif
-
-#ifndef INET6_ADDRSTRLEN
-#define INET6_ADDRSTRLEN 46
-#endif
-
 #endif // SEABOLT_CONFIG_IMPL
