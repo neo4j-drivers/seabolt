eef9d2de9a326f5e6927adaae45e71d7fc05b51d
Ali Ince <ali.ince@codart.co.uk>
2018-02-08 07:06:46 +0000

    Add boltkit integration and run tests in powershell

--- run_tests.cmd
+++ run_tests.cmd
@@ -1,6 +1,6 @@
 @Echo Off
 
-Call %~dp0\make_debug.cmd || Goto :EOF
+Call %~dp0\make_debug.cmd || Goto :Exit
 %~dp0\build\bin\Debug\seabolt-test.exe %* || Goto :Failure
 Exit /b 0
 
@@ -8,4 +8,6 @@ Exit /b 0
 	If Not "%TEAMCITY_PROJECT_NAME%" == "" (
 		ECHO ##teamcity[buildStatus status='FAILURE' text='test execution failed']
 	)
-	Goto :EOF
+
+:Exit
+	Exit /b 1
