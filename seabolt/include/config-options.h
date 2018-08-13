afdd05b4a1fae083667d84249515ed83ce195dc0
Ali Ince <ali.ince@codart.co.uk>
2018-08-13 10:51:43 +0100

    Update license headers

--- seabolt/include/config-options.h
+++ seabolt/include/config-options.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2002-2017 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ * Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j.
  *
