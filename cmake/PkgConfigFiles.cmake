b1bbba5244b622fd77f3bef0852d4e7e8d71fbbf
Ali Ince <ali.ince@codart.co.uk>
2018-10-30 10:02:50 +0000

    Fix a couple of issues and skip static pkgconfig file on windows

--- cmake/PkgConfigFiles.cmake
+++ cmake/PkgConfigFiles.cmake
@@ -2,14 +2,18 @@ message(STATUS "Passed ${LIBRARIES}")
 
 set(LIBS_PROCESSED "")
 foreach (lib ${LIBRARIES})
+    if (lib MATCHES "^\\$")
+        continue()
+    endif ()
+
     if (lib MATCHES "^-l")
         string(SUBSTRING ${lib} 2 -1 lib)
     endif ()
 
     if (lib MATCHES ".*\\.a$")
-        string(APPEND LIBS_PROCESSED "${lib} ")
+        string(APPEND LIBS_PROCESSED "-l${lib} ")
     elseif (lib MATCHES ".*\\.lib$")
-        string(APPEND LIBS_PROCESSED "${lib} ")
+        string(APPEND LIBS_PROCESSED "-l${lib} ")
     else ()
         string(APPEND LIBS_PROCESSED "-l${lib} ")
     endif ()
@@ -18,20 +22,7 @@ endforeach ()
 message(STATUS "Processed ${LIBS_PROCESSED}")
 
 configure_file(
-        ${CMAKE_CURRENT_LIST_DIR}/seabolt.pc.in
+        ${INPUT_FILE}
         ${OUTPUT_FILE}
         @ONLY
 )
-#file(GENERATE
-#        OUTPUT ${OUTPUT_FILE}
-#        CONTENT "\
-#name=${NAME}\n\
-#libdir=\${pcfiledir}/${LIBDIR}\n\
-#includedir=\${pcfiledir}/${INCLUDEDIR}\n\
-#\n\
-#Name: ${NAME}\n\
-#Description: The C Connector Library for Neo4j\n\
-#Version: ${VERSION}\n\
-#CFlags: -I\${includedir}\n\
-#Libs: -L\${libdir} -l\${name}\n\
-#")
