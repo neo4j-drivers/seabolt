44db03d42dba3f1eb1e413407a6adf4e670726b8
Tom Brennan <tjb1982@gmail.com>
2018-12-01 09:13:10 -0500

    Addl docker CI tooling to allow testing w/o TeamCity

--- ci/linux.sh
+++ ci/linux.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+
 BASE=$(dirname $0)
+
+if [ "x${NEOCTRL_LOCAL_PACKAGE}" != "x" ]; then
+    NLPPATH=/tmp/neo4j/${NEOCTRL_LOCAL_PACKAGE}
+    mkdir -p $(dirname $NLPPATH) && cp ${NEOCTRL_LOCAL_PACKAGE} ${NLPPATH}
+    NLPVOLUME="$(dirname $NLPPATH):/opt/neo4j"
+    NEOCTRL_LOCAL_PACKAGE="/opt/neo4j/$(basename $NLPPATH)"
+    DOCKER_RUN_ARGS="${DOCKER_RUN_ARGS} -v ${NLPVOLUME} --env NEOCTRL_LOCAL_PACKAGE=${NEOCTRL_LOCAL_PACKAGE}"
+fi
 
 docker image build -t seabolt-ci -f $BASE/linux/Dockerfile.build $BASE/..
 if [[ "$?" -ne "0" ]]; then
@@ -11,7 +21,7 @@ fi
 docker container run --rm --env TEAMCITY_HOST="$TEAMCITY_HOST" --env TEAMCITY_USER="$TEAMCITY_USER" \
     --env TEAMCITY_PASSWORD="$TEAMCITY_PASSWORD" --env NEOCTRLARGS="$NEOCTRLARGS" \
     --env BOLT_PORT="7687" --env HTTP_PORT="7474" --env HTTPS_PORT="7473" \
-    --env BOLT_PASSWORD="password" seabolt-ci
+    --env BOLT_PASSWORD="password" $DOCKER_RUN_ARGS seabolt-ci
 if [[ "$?" -ne "0" ]]; then
     echo "FATAL: docker container run failed, possible test failure."
     exit 1
