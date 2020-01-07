81adbe9c1757108cd8fdaf67970bdfc9a4ccabed
xuyong <yongxu@qiyi.com>
2020-01-07 18:41:31 +0800

    support other xcode version

--- src/seabolt-cli/src/main.c
+++ src/seabolt-cli/src/main.c
@@ -39,6 +39,27 @@
 
 #define UNUSED(x) (void)(x)
 
+#ifdef __APPLE__
+#include <mach/clock.h>
+#include <mach/mach.h>
+
+#ifndef TIME_UTC
+#define TIME_UTC 0
+
+void timespec_get(struct timespec *ts, int type)
+{
+    UNUSED(type);
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    ts->tv_sec = mts.tv_sec;
+    ts->tv_nsec = mts.tv_nsec;
+}
+#endif
+#endif
+
 enum Command
 {
     CMD_NONE,
